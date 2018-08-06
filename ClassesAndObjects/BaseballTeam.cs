d59f16a935ff0903e6d2a6ae0dfc5160337ac1bc
Tomas Medici <tmi@breakoutimage.dk>
2018-08-06 16:54:27 +0200

    sadf

---
+++ ClassesAndObjects/BaseballTeam.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndObjects
+{
+    class BaseballTeam
+    {
+        private string name;
+        private string stadium;
+        private int wins = 0;
+        private int defeats = 0;
+
+        public BaseballTeam(string n, string s, string f)
+        {
+            this.name = n;
+            this.stadium = s;
+            this.field = f;
+        }
+    }
+}
---
+++ ClassesAndObjects/BaseballTeam.vb
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndObjects
+{
+    class BaseballTeam
+    {
+        Private Name As String;
+        Private Stadium As String;
+
+
+    }
+}
